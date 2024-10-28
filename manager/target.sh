702e3837e62779551873f6b3be6ce562a84fb21e
inha-cvl <cvlab.inhauniv@gmail.com>
2024-10-28 19:07:18 +0900

    avante

--- manager/target.sh
+++ manager/target.sh
@@ -3,13 +3,13 @@
 cd ../sharing_info
 python3 main.py target Solbat 0&
 cd ../v2x
-python3 main.py target 2 &
+python3 main.py target 0 &
 cd ../ui/visualizer
 python3 visualizer.py target &
 cd ../
 python3 ui.py target &
 cd ../utils
-python3 obigo_control.py target&
+#python3 obigo_control.py target &
 python3 make_data.py target
 
 # cd ~/Documents
