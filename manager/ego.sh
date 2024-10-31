75da5d8694d585202119fc53e6d4fa94bfcb031b
kka-na <kimgana96@gmail.com>
2024-10-31 12:25:42 +0900

    [manager] update

--- manager/ego.sh
+++ manager/ego.sh
@@ -10,9 +10,8 @@ cd ../
 python3 ui.py ego &
 cd ../utils
 python3 obigo_control.py ego &
-python3 make_data.py ego
-
-# cd plotjuggler/
-# rosrun plotjuggler plotjuggler -l communication_performance.xml &
-# cd ~/Documents
-# rosbag record /ego/CommunicationPerformance /ego/EgoShareInfo /ego/TargetShareInfo /mobinha/perception/lidar/track_box /novatel/oem7/inspva /novatel/oem7/odom /gmsl_camera/dev/video0/compressed /ego/dangerous_obstacle /ego/user_input /ego/simulator_input
+python3 make_data.py ego &
+cd plotjuggler/
+rosrun plotjuggler plotjuggler -l communication_performance.xml &
+cd ~/Documents
+rosbag record /ego/CommunicationPerformance /ego/EgoShareInfo /ego/TargetShareInfo /mobinha/perception/lidar/track_box /novatel/oem7/inspva /novatel/oem7/odom /gmsl_camera/dev/video0/compressed /ego/dangerous_obstacle /ego/user_input /ego/simulator_input 
